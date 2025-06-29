e85984c83550a2357978488f71b2f7ee89248ee2
Chetan Khadke <chetankhadke000@gmail.com>
2025-06-29 16:12:29 +0530

    Name Updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,7 +17,7 @@ const SearchBar = ({city, weather, setCity, setWeather, searchWeather}) => {
 
   return (
     <div>
-        <h2>Weather App</h2>
+        <h2>Skynote</h2>
 
         <form onSubmit={handleSubmit}>
             <Box
